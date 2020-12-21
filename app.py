ba0a33105f538b6044e19f620b5ff1894752a443
SharathChampzz <sharathkumar.mskj.1999@gmail.com>
2020-12-21 21:52:31 +0530

    added try except block to all functions

--- app.py
+++ app.py
@@ -21,49 +21,60 @@ def getprojectname(url):
 
 @app.route("/")
 def index():
-	print('/')
-	return render_template("uploader.html") # upload to uploader
-
+	try:
+		print('/')
+		return render_template("uploader.html") # upload to uploader
+	except Exception as e:
+		return e
 
 @app.route('/takeurl')
 def take(): # calling this function and changing url
-	return render_template('takeurl.html') # /takeinput to /takeurl
+	try:
+		return render_template('takeurl.html') # /takeinput to /takeurl
+	except Exception as e:
+		return e
 
 @app.route('/takeurl', methods=["GET", "POST"]) # /takeinput to /takeurl
 def takk(): # to accept url of firebase
-	print('/takeurl')
-	global file_name
-	url = request.form['firebaseurl']
+	try:
+		print('/takeurl')
+		global file_name
+		url = request.form['firebaseurl']
 
-	print(f'Filename or JSOn Name : {file_name}')
-	print(f'URL  : {url}')
+		print(f'Filename or JSOn Name : {file_name}')
+		print(f'URL  : {url}')
 
-	print(file_name + '\n' + url)
-	adduser(getprojectname(url), file_name.split('.')[0], url, 'Added Just Now!')
-	# print(getusers())
-	# return '<h1 style="color:green">Added Successfully..!</h1>'
-	return render_template('sucess.html')
+		print(file_name + '\n' + url)
+		adduser(getprojectname(url), file_name.split('.')[0], url, 'Added Just Now!')
+		# print(getusers())
+		# return '<h1 style="color:green">Added Successfully..!</h1>'
+		return render_template('sucess.html')
+	except Exception as e:
+		return e
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():  # to accept json file | file uploader
-	print('/upload')
-	global file_name
+	try:
+		print('/upload')
+		global file_name
 
-	target = os.path.join(APP_ROOT, 'static')
-	# print(f'Target : {target}')
+		target = os.path.join(APP_ROOT, 'static')
+		# print(f'Target : {target}')
 
-	if not os.path.isdir(target):
-		os.mkdir(target)
-	
-	if request.method =='POST':
-		file = request.files['file']
-		if file:
-			filename = secure_filename(file.filename)
-			print(filename)
-			file_name = filename
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-			return redirect(url_for('take'))
-	return 'Some thing Wrong Happened File not found may be'
+		if not os.path.isdir(target):
+			os.mkdir(target)
+		
+		if request.method =='POST':
+			file = request.files['file']
+			if file:
+				filename = secure_filename(file.filename)
+				print(filename)
+				file_name = filename
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+				return redirect(url_for('take'))
+		return 'Some thing Wrong Happened File not found may be'
+	except Exception as e:
+		return e
 
 
 @app.route('/<jsonno>/<path>/<readings>') 
